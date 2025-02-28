6b3cdc5940f5b77c690932bc62ed4b755073b5dc
Arkhamk <163548881+Arkhamk@users.noreply.github.com>
2025-02-28 18:02:17 +0800

    Update VS_Block.py
    卷积层改线性层的VAE

--- unet/VS_Block.py
+++ unet/VS_Block.py
@@ -6,16 +6,28 @@ import torch.nn.functional as F
 # 变分采样，增大域视野
 class VariationalSampling(nn.Module):
 
-    def __init__(self, in_channels, latent_dim):
+    def __init__(self, in_channels, gaussian_dim):
         super(VariationalSampling, self).__init__()
         self.in_channels = in_channels
-        self.latent_dim = latent_dim
+        self.gaussian_dim = gaussian_dim
+        self.flat_dim = in_channels*2*2
 
         # 编码器输出均值和方差
-        self.mu_layer = nn.Conv2d(in_channels, latent_dim, kernel_size=1)
-        self.logvar_layer = nn.Conv2d(in_channels, latent_dim, kernel_size=1)
+        self.mu_layer = nn.Linear(self.flat_dim, self.gaussian_dim)
+        self.logvar_layer = nn.Linear(self.flat_dim, self.gaussian_dim)
+
+        # 解码层
+        self.decoder=nn.Sequential(
+            nn.Linear(gaussian_dim, self.in_channels),
+            nn.Tanh(),
+            nn.Linear(self.in_channels, self.flat_dim),
+            nn.Sigmoid() #图片被转为为0，1的值了，故用此函数
+        )
 
     def forward(self, x):
+        # (N,Ci,2,2)转(N,Ci*2*2)
+        x = torch.flatten(x, 1)
+
         # 计算均值和方差       
         mu = self.mu_layer(x)
         logvar = self.logvar_layer(x)
@@ -24,5 +36,9 @@ class VariationalSampling(nn.Module):
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
         z = mu + eps * std
+
+        # 解码回(N,Ci,2,2)
+        z = self.decoder(z)
+        z = z.reshape(-1, self.in_channels, 2, 2)
 
         return z, mu, logvar
