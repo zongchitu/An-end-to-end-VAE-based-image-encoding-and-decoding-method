d92e14c854859d3841741a625b4b0d9d93b3e249
Pengyu Li <202421044678@mail.scut.edu.cn>
2025-03-01 23:09:28 +0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,16 @@
+import os
+import random
+import torch
+import numpy as np
+
+
+# 使实验具备可重复性，固定种子
+def seed_all(seed=42):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
